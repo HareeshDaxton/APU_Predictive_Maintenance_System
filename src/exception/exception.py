679f780b3cd509da05821319baeae93b5dc27e38
Ashok13082804 <ashoksivakumar2804@gmail.com>
2026-01-30 16:03:05 +0530

    initial tracking

---
+++ src/exception/exception.py
@@ -0,0 +1,13 @@
+import sys
+from src.Logging import logger
+
+class CustomException(Exception):
+    def __init__(self, error_msg, error_detail:sys):
+        self.error_msg = error_msg
+        exc_type, exc_value, exc_tb = error_detail.exc_info()
+        
+        self.line_no = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+        
+    def __str__(self):
+        return f"Error occured in python script name ==> [{self.file_name}] line number ==> [{self.line_no}] error message ==> [{self.error_msg}]"
