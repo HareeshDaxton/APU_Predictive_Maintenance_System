433ee5a765bbab45a3b4bbe759e1d3e966a686ff
HareeshDaxton <hareeshdaxton@gmail.com>
2026-02-14 22:01:12 +0530

    bug fix in Data_split and Artifacts

--- src/Data_preprocessing/Data_split/Data_split.py
+++ src/Data_preprocessing/Data_split/Data_split.py
@@ -1,4 +1,4 @@
-from aiohttp_retry import Tuple
+import numpy as np
 from Utils.Exception.exception import CustomException
 from Utils.Logging.logger import logger
 import yaml
@@ -26,6 +26,8 @@ class DataSplit:
                 logger.error(f'Error loading configuration =>src/Data_preprocessing/Data_split/Data_split.py => _load_config() => {e}')
                 raise CustomException('Error loading configuration', sys)
             
+            
+# This splitting is done before feature engineering to prevent data leakage.
     def run_data_split(self):
             try:
                 logger.info('Splitting data initiated')
@@ -35,27 +37,44 @@ class DataSplit:
                 # Split by engine_id to prevent data leakage.
                 # Ensures entire engine histories are kept either in train or test.
                 # This simulates real-world scenario where model predicts on unseen engines.
+                
+                # Get unique engines
                 unique_engines = df["engine_id"].unique()
+                
+                
+                # Shuffle engines (industry standard)
+                np.random.seed(42)                                              
+                np.random.shuffle(unique_engines)       # NOTE only randomizing which engines go to train and test.    
+                logger.info('"engine_id" are shuffeled')
+                
                 split_index = int(len(unique_engines) * (1 - self.test_size))
                 
                 logger.info("Splitting data into train and test sets based on engine_id")
-                train_engines = unique_engines[:split_index]
+                train_engines = unique_engines[:split_index]              
                 test_engines = unique_engines[split_index:]
                 logger.info("Splitting data is over")
                 
-                train__df = df[df['engine_id'].isin(train_engines)]
-                test_df = df[df['engine_id'].isin(test_engines)]
+                #Check id 'engine_id' is in train or test set and split accordingly
+                train_df = df[df['engine_id'].isin(train_engines)]           
+                test_df = df[df['engine_id'].isin(test_engines)]             
                                 
+                logger.info(f"Train engines: {len(train_engines)}")
+                logger.info(f"Test engines: {len(test_engines)}")
+
+                logger.info(f"Train rows: {train_df.shape[0]}")
+                logger.info(f"Test rows: {test_df.shape[0]}")
+            
                 logger.info('Saving train and test datasets')
                 os.makedirs(os.path.dirname(self.train_output_path), exist_ok=True)
                 os.makedirs(os.path.dirname(self.test_output_path), exist_ok=True)
                 
-                train__df.to_csv(self.train_output_path, index=False)
-                test_df.to_csv(self.test_output_path, index=False)
+                train_df.to_csv(self.train_output_path, index=False)             #splitted into train csv
+                test_df.to_csv(self.test_output_path, index=False)                #splitted into test csv
                 logger.info('Train and Test set is saved successfully')
                 
             except Exception as e:
                 logger.error(f'Error during data splitting => src/Data_preprocessing/Data_split/Data_split.py => run_data_split() => {e}')
                 return CustomException('Error during data splitting', sys)
             
-            print(df.shape)
+# Note: The data splitting is done before feature engineering to prevent data leakage.
+# Next feature engineering (ipynb to py)
