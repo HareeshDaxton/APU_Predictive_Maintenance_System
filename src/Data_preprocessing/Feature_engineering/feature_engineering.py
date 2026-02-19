087f8e05418d3a457d0436b91cd30344d81675f8
HareeshDaxton <hareeshdaxton@gmail.com>
2026-02-19 16:27:13 +0530

    update of FE

--- src/Data_preprocessing/Feature_engineering/feature_engineering.py
+++ src/Data_preprocessing/Feature_engineering/feature_engineering.py
@@ -1,7 +1,39 @@
+import os
+import numpy as np
+import sys
+import pandas as pd
+import yaml
+from Utils.Exception.exception import CustomException
+from Utils.Logging.logger import logging
 
 
-
-
+class FeatureEngineering:
+    def __init__(self, config_path="config/config.yaml"):
+        self.config_path = config_path
+        self.config = self._load_config()
+        
+        self.train_data_path = self.config['data']['train_data_path']
+        self.test_data_path = self.config['data']['test_data_path']
+        
+        self.preprocessed_train_path = self.config['data']['train_preprocessed_path']
+        self.preprocessed_test_path = self.config['data']['test_preprocessed_path']
+        
+        self.scaler_path = self.config['data']['scaler_path']
+        
+        self.sensor_columns = [
+            'sensor_2','sensor_3','sensor_4','sensor_7','sensor_8',
+            'sensor_9','sensor_11','sensor_12','sensor_13','sensor_14',
+            'sensor_15','sensor_17','sensor_20','sensor_21'
+        ]
+        
+    def _load_config(self):
+        try:
+            with open(self.config_path, 'r') as f:
+                return yaml.safe_load(f)
+        except Exception as e:
+            raise CustomException(f"Error loading config file: {e}")
+        
+    
 
 
 
